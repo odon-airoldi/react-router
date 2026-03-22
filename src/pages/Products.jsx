115abdf7323e4411f8cd03f008a6bd5757bf6923
odon-airoldi <odon.airoldi@gmail.com>
2026-03-22 14:57:15 +0100

    add product in products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -23,7 +23,30 @@ export default function Products() {
         <>
             <main>
                 <div className="container py-5">
-                    Products
+
+                    <div className="row row-cols-4 g-3">
+
+                        {
+                            products.map(product => (
+
+                                <div className="" key={product.id}>
+                                    <div className="card">
+                                        <div className="ratio ratio-1x1">
+                                            <img src={product.image} className="card-img-top object-fit-contain img-fluid p-3" alt={product.title} />
+                                        </div>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{product.title}</h5>
+                                            <p className="card-text">{product.description.slice(0, 80)}...</p>
+                                            <a href="#" className="btn btn-primary">buy {product.price} €</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            ))
+                        }
+
+                    </div>
+
                 </div>
             </main>
         </>
